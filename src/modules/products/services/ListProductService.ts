4a17ee9d0ba6ba1d652cb9ab8cb561d420bee01d
Gabriel <koymattk@gmail.com>
2021-02-27 16:30:38 -0300

    creating product listing service

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,14 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import ProductRepository from '../typeorm/repositories/ProductRepository';
+
+class ListProductService {
+    async execute(): Promise<Product[]> {
+        const productRepository = getCustomRepository(ProductRepository);
+        const products = await productRepository.find();
+        return products;
+    }
+}
+
+export default new ListProductService();
