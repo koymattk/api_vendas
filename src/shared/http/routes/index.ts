69746d5662ae749bdc35ac2922740ec3a94fa174
Gabriel <koymattk@gmail.com>
2021-02-27 18:31:17 -0300

    adicionando product router ao arquivo de rotas principal

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
+import productRoutes from '@modules/products/routes/products.routes';
 
 const routes = Router();
-
+routes.use('/products', productRoutes);
 routes.get('/', (req, res) => {
     return res.json({ message: 'olá express' });
 });
-
 export default routes;
