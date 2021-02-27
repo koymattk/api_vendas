fb34c8e6e8666d81825790c5d4a9a11781f6aa53
Gabriel <koymattk@gmail.com>
2021-02-27 14:16:22 -0300

    correcting path error and adding typeorm with postgres

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,26 @@
-import express from 'express';
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
 import cors from 'cors';
+import express, { Request, Response } from 'express';
 import routes from './routes';
+import '@shared/typeorm';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+        });
+    }
+    return res.status(500).json({
+        error: 'error',
+        message: 'internal server error',
+    });
+});
 
-app.listen(3000, () => console.log("Running on port 3000"));
+app.listen(3000, () => console.log('Running on port 3000'));
