510c0e1a92687563f7aa97246b0065794fb75683
Gabriel <koymattk@gmail.com>
2021-02-27 16:38:49 -0300

    creating service to seek a single product

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,4 +1,3 @@
-import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductRepository';
@@ -6,7 +5,9 @@ import ProductRepository from '../typeorm/repositories/ProductRepository';
 class ListProductService {
     async execute(): Promise<Product[]> {
         const productRepository = getCustomRepository(ProductRepository);
+
         const products = await productRepository.find();
+
         return products;
     }
 }
