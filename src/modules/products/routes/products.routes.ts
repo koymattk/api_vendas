41d755330cc85ed9285ff6551c0377f304665750
Gabriel <koymattk@gmail.com>
2021-02-27 20:28:58 -0300

    adicionando celebrate

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,52 @@
 import { Router } from 'express';
-import { deflate } from 'zlib';
 import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productRoutes = Router();
 
 productRoutes.get('/', ProductsController.index);
-productRoutes.get('/:id', ProductsController.show);
-productRoutes.post('/', ProductsController.create);
-productRoutes.put('/:id', ProductsController.update);
-productRoutes.delete('/:id', ProductsController.delete);
+productRoutes.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    ProductsController.show,
+);
+productRoutes.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().required().precision(2),
+            quantity: Joi.number().required(),
+        },
+    }),
+    ProductsController.create,
+);
+productRoutes.put(
+    '/:id',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().required().precision(2),
+            quantity: Joi.number().required(),
+        },
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    ProductsController.update,
+);
+productRoutes.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    ProductsController.delete,
+);
 
 export default productRoutes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import AppError from '@shared/errors/AppError';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 import routes from './routes';
 import '@shared/typeorm';
@@ -11,7 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use((error: Error, req: Request, res: Response) => {
+app.use(errors());
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof AppError) {
         return res.status(error.statusCode).json({
             status: 'error',
