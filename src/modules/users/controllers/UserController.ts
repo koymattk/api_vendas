7be3e9d70da74a5b46e8394bc431db3350f16b8e
Gabriel <koymattk@gmail.com>
2021-03-02 14:20:06 -0300

    create a controller of user

---
+++ src/modules/users/controllers/UserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUserService from '../services/ListUserService';
+class UserController {
+    public async create(req: Request, res: Response): Promise<Response> {
+        const { name, email, password } = req.body;
+
+        const user = await CreateUserService.execute({ name, email, password });
+
+        return res.json(user);
+    }
+    public async listUsers(req: Request, res: Response): Promise<Response> {
+        const users = await ListUserService.execute();
+        return res.json(users);
+    }
+}
--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -7,8 +7,10 @@ class ListUserService {
     public async execute(): Promise<User[]> {
         const userRepository = getCustomRepository(UserRepository);
 
-        const users = userRepository.find();
+        const users = await userRepository.find();
 
         return users;
     }
 }
+
+export default new ListUserService();
